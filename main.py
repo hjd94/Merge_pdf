2e63ed680ee702c5beea425bbd78110b764e21b5
hjd94 <112848877+hjd94@users.noreply.github.com>
2022-09-08 18:49:26 +1200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ mypath = r''
 # Call the PdfFileMerger
 merging_object = PyPDF2.PdfFileMerger()
 
-# read directory
-supporting_docs = [file for file in listdir(mypath) if isfile(join(mypath, file))]
-
+# read contents of folds
+supporting_docs = [file for file in listdir(mypath) if file.endswith(".pdf")]
 for doc in supporting_docs:
     merging_object.append(PdfFileReader(doc, 'rb'))
-
-
 # Write all the files into a file which is named as shown below
 merging_object.write("mergedfilesoutput.pdf")
